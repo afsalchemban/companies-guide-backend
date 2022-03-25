d6bf27492b3833bbe048bc3814ef9fb9a571d598
afsalchemban <afsalchemban@gmail.com>
2022-03-25 11:16:38 +0400

    migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory()->state([
+        DB::table('users')->insert([
             'name' => 'Admin',
             'email' => 'admin@admin.com',
-            'user_type' => 'admin',
-        ])->create();
+            'email_verified_at' => now(),
+            'password' => bcrypt('dummypassword'), // password
+            'remember_token' => Str::random(10),
+        ]);
         DB::unprepared("INSERT INTO `company_activities` (`id`, `title`) VALUES
         (1, 'Agriculture, hunting and related service activities'),
         (2, 'Agriculture, Growing Vegetables  Plant Nurseries Product'),
