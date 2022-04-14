7f0c0f32adab051ea310d0b5c5838d5729676bfd
afsalchemban <afsalchemban@gmail.com>
2022-04-14 14:54:09 +0400

    Sale report for sale user

--- app/Services/Reports/Sale/SaleReportForSale.php
+++ app/Services/Reports/Sale/SaleReportForSale.php
@@ -5,6 +5,7 @@ namespace App\Services\Reports\Sale;
 use App\Http\Resources\SaleReportResource;
 use App\Interfaces\SaleReportInterface;
 use App\Models\Sale;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\Auth;
 
 class SaleReportForSale extends SaleReport implements SaleReportInterface 
@@ -13,12 +14,12 @@ class SaleReportForSale extends SaleReport implements SaleReportInterface
     private function _extract()
     {
         $sale = Auth::user()->userable;
-        return SaleReportResource::collection(Sale::withCount('companies')->where('id',$sale->id)->get());
-    }
+        return SaleReportResource::collection(Sale::withCount(['companies' => function (Builder $query) {
 
-    private function _filterBySale(){
-        $sale = Sale::find($this->sale);
-        return new SaleReportResource($sale);
+            if($this->package!=null)
+            { $query->where('package_id',$this->package); }
+
+        }])->where('id',$sale->id)->get()); 
     }
 
     public function generate()
